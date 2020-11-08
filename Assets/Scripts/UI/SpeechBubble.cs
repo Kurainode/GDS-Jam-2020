19d16c39c779ab7fe52006fcecb2839367afefaa
Pierre Pinet <kurainode@gmail.com>
2020-11-08 14:08:29 +0100

    oui

--- Assets/Scripts/UI/SpeechBubble.cs
+++ Assets/Scripts/UI/SpeechBubble.cs
@@ -10,22 +10,26 @@ public class SpeechBubble : MonoBehaviour
     public float textSpeed;
 
     private float m_carret;
+    private AudioSource m_audioSource;
 
     void Start()
     {
         m_carret = 0;
         content.text = text;
+        m_audioSource = GetComponent<AudioSource>();
     }
     
     void Update()
     {
         if (m_carret < text.Length)
         {
-            //int old = (int)m_carret;
+            int old = (int)m_carret;
             m_carret += Time.deltaTime * textSpeed;
             content.text = text.Substring(0, (int)m_carret);
-            //if (old < (int)m_carret)
-            //    GetComponent<TriggerOnSpeak>().Speak();
+            if (old < (int)m_carret)
+            {
+                m_audioSource.Play();
+            }
         }
     }
 }
